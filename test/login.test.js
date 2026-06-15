5ad3963c48d741c7f31968d28a4fe5edf43f51f4
Caio Marques <cmls.marques@gmail.com>
2026-06-15 13:56:28 -0300

    Subindo o primeiro teste automatizado em js

---
+++ test/login.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const {expect} = require('chai');
+//describe Metodo do Mocha para descrever um agrupamento de testes.
+//utilizando arrow function ()=>{}
+describe ('Login',() => {
+    describe ('POST /login', () => {
+        it ('Deve retornar 200 com token em string quando usar credenciais válidas', async()=>{
+            //Vai usar o SUPERTEST, para realizar uma requisição a API de login para retornar o TOKEN
+            //Formas de fazer requisição com SUPERTEST
+            const resposta = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json') //setando o cabeçalho para a requisição
+                .send({
+                        'username': 'julio.lima',
+                        'senha': '123456'
+                    })
+
+            //Fazendo validações com o CHAI
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.token).to.be.a('string');
+
+        })
+    })
+})
