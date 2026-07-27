f24aaf1485f642e452cf09227e4dd397a63d2f96
Caio Marques <cmls.marques@gmail.com>
2026-07-27 14:10:30 -0300

    adicionando ajuste de valor no teste de transferencia

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -59,7 +59,7 @@ describe('Transferencias',() =>{
                 expect(resposta.body.id).to.be.a('number') // Esse valida a tipagem do valor
                 expect(resposta.body.conta_origem_id).to.equal(1)
                 expect(resposta.body.conta_destino_id).to.equal(2)
-                expect(resposta.body.valor).to.equal(11.00)
+                expect(resposta.body.valor).to.equal("11.00")
 
         })
     })
